43d4ac0fa43a0db4198e73c4aed99f39093f01fd
Leonardo Gatica <lgatica@protonmail.com>
2019-10-17 12:11:47 -0300

    feat(semantic-release): migrate to plugins config

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 module.exports = {
-  verifyConditions: [
+  plugins: [
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
     '@semantic-release/gitlab',
     'semantic-release-gitlab-registry',
     'semantic-release-rancher'
-  ],
-  prepare: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/git'
-  ],
-  publish: [
-    '@semantic-release/npm',
-    '@semantic-release/gitlab',
-    'semantic-release-gitlab-registry',
-    'semantic-release-rancher'
-  ],
-  success: false,
-  fail: false
+  ]
 }
